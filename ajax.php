0e4f20db524951036a9079cad8469043284fed01
okard <d4rkdragon@gmail.com>
2013-11-03 15:08:45 +0100

    fix debug output in ajax functions

--- ajax.php
+++ ajax.php
@@ -11,8 +11,8 @@ function afmng_ajax_get_releases()
 	
 	$data = afmng_db_project_releases($project_id);
 	
+	ob_clean();
 	echo json_encode($data);
-	
 	die(); // this is required to return a proper result
 }
 
@@ -24,6 +24,7 @@ add_action('wp_ajax_get_steps', 'afmng_ajax_get_steps');
 */ 
 function afmng_ajax_get_steps()
 {
+	ob_clean();
 	echo json_encode(afmng_db_steps());
 	die();
 }
