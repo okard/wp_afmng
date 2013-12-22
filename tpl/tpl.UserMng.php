e0ef6329ba25bf67141ca5e9896830042128f8a3
rcameise <simon.zettel@gmx.ch>
2013-12-22 19:21:38 +0100

    layout

---
+++ tpl/tpl.UserMng.php
@@ -0,0 +1,50 @@
+<div id="afmng_content">
+
+<div id="afmng_header">
+User Manager
+</div>
+
+<div id="afmng_body">
+<<<<<<< HEAD
+=======
+<h1>User Manager</h1>
+
+>>>>>>> upstream/master
+<form id="update_user_caps" method="post" action="">
+	<table class="afmng_table">
+		<thead>
+			<tr>
+				<th>User</th>
+				<?php foreach($this->caps as $cap): ?>
+					<th><?php echo $cap; ?></th>
+				<?php endforeach; ?>
+			</tr>
+		</thead>
+		
+		<?php foreach($this->users as $user): ?>
+			<tr>
+				<td><?php echo $user->user_login; ?></td>
+				<?php foreach($this->caps as $cap): ?>
+					<td>
+						<input type="checkbox" name="<?php echo $cap; ?>:<?php echo $user->ID; ?>" <?php echo afmng_user_cap($cap, $user->ID) ? 'checked' : ''; ?>>
+					</td>
+				<?php endforeach; ?>
+			</tr>
+		<?php endforeach; ?>
+		
+	</table>
+<<<<<<< HEAD
+	<br>
+=======
+	
+>>>>>>> upstream/master
+	<input type="hidden" name="action" value="update_user" />
+	<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Speichern') ?>" />
+	
+</form>
+
+</div>
+<<<<<<< HEAD
+</div>
+=======
+>>>>>>> upstream/master
